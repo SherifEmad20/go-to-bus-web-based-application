8743806356d13a2808402c3b2946927a6cf18181
HussienSayed14 <Hussiens399@gmail.com>
2022-05-31 18:29:22 +0200

    User Entity

---
+++ GoToBus/Bus_Ejbb/ejbModule/EJBs/User.java
@@ -0,0 +1,84 @@
+package EJBs;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Stateless
+@LocalBean
+@Entity
+@Table(name="USER")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+
+	int id;
+	@Column(name="username")
+	public String username;
+	@Column(name="password")
+
+	String password;
+	@Column(name="full_name")
+
+	String full_name;
+	@Column(name="role")
+
+	String role;
+	
+	
+@OneToMany
+//@JoinColumn(name="trips")
+public List<Trip> trips;
+
+@OneToMany
+public List<Notification> UserNotifications;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return username;
+	}
+	public void setUserName(String userName) {
+		username = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFull_name() {
+		return full_name;
+	}
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
