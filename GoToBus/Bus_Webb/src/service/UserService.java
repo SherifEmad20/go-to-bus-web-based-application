b2da7736989bd8c7823d743bdf3d8396cd7b1347
Sherif Emad <65064918+SherifEmad20@users.noreply.github.com>
2022-05-31 18:43:41 +0200

    Login function

--- GoToBus/Bus_Webb/src/service/UserService.java
+++ GoToBus/Bus_Webb/src/service/UserService.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package service;
 
 import java.awt.List;
@@ -56,3 +57,95 @@ public class UserService {
 		}
 	}
 	
+=======
+package service;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+//import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import EJBs.Station;
+import EJBs.User;
+
+
+
+
+
+@Stateless
+@Path("/users")
+
+public class UserService {
+	
+	boolean flag = false;
+	
+	@PersistenceContext(unitName="hello")
+	private EntityManager entitymanager;
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("user")
+	public String register(User u) 
+	{
+		try 
+		{
+			String us = u.username;
+			//User u = new User();
+			u.setUserName(us);
+			entitymanager.persist(u);
+			return "Success " ;
+		}
+			catch (Exception e)
+		{
+			throw new EJBException(e);
+		}
+	}
+	
+	
+@POST
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("login")
+public String log(User u1)
+{
+	String response;
+	String passw= u1.getPassword();
+	
+	String userN = u1.getUserName();
+	User user = entitymanager.createQuery(
+			  "SELECT u from User u WHERE u.username = :userN", User.class).
+			setParameter("userN",userN ).getSingleResult();
+	String pass = user.getPassword();
+	
+	
+	if (pass.equals(passw))
+	{
+		response = "Login successfuly";
+		flag = true;
+	}
+	
+	else
+	{
+		response = "Login Failed";
+		
+	}
+	
+	return response;
+    }
+}
+>>>>>>> 5433193 (Login function)
